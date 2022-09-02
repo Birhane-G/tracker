c34090f4fc5801abca70e5a00a531799e0ddece5
Gemechisgk <gemechis.gk@gmail.com>
2022-09-02 11:09:48 +0300

    update

---
+++ src/Layout/footer.js
@@ -0,0 +1,38 @@
+
+import React from "react"
+
+function footer() {
+    return ( 
+    <section classname="footer">
+        <div classname="footer">
+            <div classname="importantlinks">
+                <h3>Important Links</h3>
+                    <ul>
+                        <a href="">Home</a>
+                        <a href="">Blog</a>
+                        <a href="">Partners</a>
+                        <a href="">Signup</a>
+                        <a href="">Locate us</a>
+                    </ul>
+            </div>
+            <div classname="communications">
+                <ul>
+                    <a href="">Email</a>
+                    <a href="">Twitter</a>
+                    <a href="">Facebook</a>
+                    <a href="">LinkedIn</a>
+                    <a href="">Telegram</a>
+                    <a href="">Phone</a>
+                </ul>
+            </div>
+            <div classname="location">
+                <a href="">Mexico CreativeHub</a>
+            </div>
+
+
+
+        </div>
+        </section>
+    )
+}
+export default footer
