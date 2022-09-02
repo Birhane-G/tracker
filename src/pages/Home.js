8870554004d919e38f16fcf4d6a9712dcdc6cad8
Binize <binizele18@gmail.com>
2022-09-02 11:40:47 +0300

    body

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Layout/Header'
 import Footer from '../Layout/Footer'
+import Body from '../Layout/Body'
 function Home() {
   return (
     <>
     <Header />
     <h1>Home page</h1>
+    <Body />
 
     <Footer />
     </>
