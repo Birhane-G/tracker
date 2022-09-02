26d5d7b0c6767f092235281b528ad9e955ca9ea5
Birhane <Birhanegebrial1@gmail.com>
2022-09-02 15:57:04 +0300

    Birhane

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import error from "../images/error.png";
+import "../Styles/error_style.css";
 function Error() {
   return (
-    <div>Error page</div>
-  )
+  <section className="error-page">
+    <div className="error">
+      <div className="error-image">
+        <img src={error} alt="error page" />
+      </div>
+      <div className="error-text">
+        <h1>404</h1>
+        <p>Page Not Found</p>
+      </div>
+    </div>
+  </section>)
+  ;
 }
 
-export default Error
+export default Error;
