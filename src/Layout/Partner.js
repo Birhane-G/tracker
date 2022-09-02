fe3a4a89fbf249603ddc7b97e7415f9cd4882043
Birhane <Birhanegebrial1@gmail.com>
2022-09-02 16:26:14 +0300

    Birhane

--- src/Layout/Partner.js
+++ src/Layout/Partner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import earald from "../images/partners/Earald.jpg";
 
 function Partner() {
   return (
@@ -6,7 +7,7 @@ function Partner() {
       <div className="partners">
         <div className="partner-cont">
             <div className="box">
-                
+                <img src={earald} alt="" />
             </div>
         </div>
       </div>
