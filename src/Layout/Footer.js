072544b979095b9fc3895cd056b435dedef9441a
Birhane <Birhanegebrial1@gmail.com>
2022-09-02 17:08:17 +0300

    changes

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,32 +2,36 @@ import React from "react";
 import '../Styles/footer_style.css';
 function Footer() {
   return (
-    <section id="footer">
-      <div classname="footer-cont">
-        <div classname="importantlinks">
+    <section className="footer">
+      <div>
+      <div className="footer-cont">
+        <div className="importantlinks">
           <h3>Important Links</h3>
           <ul>
-            <a href="/">Home</a>
-            <a href="/">Blog</a>
-            <a href="/">Partners</a>
-            <a href="/">Signup</a>
-            <a href="/">Locate us</a>
+            <a href="/"><li>Home</li></a>
+            <a href="/"><li>Partners</li></a>
+            <a href="/"><li>Service</li></a>
+            <a href="/"><li>About us</li></a>
+            <a href="/"><li>Help</li></a>
+            <a href="/"><li>Locate us</li></a>
           </ul>
         </div>
-        <div classname="communications">
+        <div className="communications">
+          <h3>communications</h3>
           <ul>
-            h3
-            <a href="/">Email</a>
-            <a href="/">Twitter</a>
-            <a href="/">Facebook</a>
-            <a href="/">LinkedIn</a>
-            <a href="/">Telegram</a>
-            <a href="/">Phone</a>
+            <a href="/"><li>Email</li></a>
+            <a href="/"><li>Twitter</li></a>
+            <a href="/"><li>Facebook</li></a>
+            <a href="/"><li>LinkedIn</li></a>
+            <a href="/"><li>Telegram</li></a>
+            <a href="/"><li>Phone</li></a>
           </ul>
         </div>
-        <div classname="location">
+        <div className="location">
           <a href="/">Mexico CreativeHub</a>
         </div>
+      </div>
+         
       </div>
     </section>
   );
