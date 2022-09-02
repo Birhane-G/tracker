b43ed36aaf3e778a8ca2293f9a12ee231fd4e9cd
Birhane <Birhanegebrial1@gmail.com>
2022-09-02 12:03:27 +0300

    Birhane changes

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,24 +1,34 @@
 import React from "react";
 import logo from "../images/Trackerlogo.png";
-
+import "../Styles/header_style.css";
 function Header() {
   return (
     <header>
       <div className="Header-cont">
         <div className="logo">
-            <img src={logo} alt="Tracker Logo" />
-            <h1>Tracker</h1>
+          <img src={logo} alt="Tracker Logo" />
+          <h1>Tracker</h1>
         </div>
-        <div className='navbar'>
-        <nav className='navigation'>
-          <ul>
-            <a href='/'><li>Home</li></a>
-            <a href='/'><li>Portifolio</li></a>
-            <a href='/'><li>Service</li></a>
-            <a href='/'><li>About us</li></a>
-            <a href='/'><li>Contact</li></a>
-        </ul>
-      </nav>
+        <div className="navbar">
+          <nav className="navigation">
+            <ul>
+              <a href="/">
+                <li>Home</li>
+              </a>
+              <a href="/">
+                <li>Portifolio</li>
+              </a>
+              <a href="/">
+                <li>Service</li>
+              </a>
+              <a href="/">
+                <li>About us</li>
+              </a>
+              <a href="/">
+                <li>Contact</li>
+              </a>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
