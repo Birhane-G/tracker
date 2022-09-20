804bb30d8a2165277801ebac4b1fc50e03517751
Birhane <Birhanegebrial1@gmail.com>
2022-09-20 14:43:45 +0300

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,34 +3,34 @@ import "../Styles/login_style.css";
 import logo from "../images/Trackerlogo.png";
 function Login() {
   return (
-    <section className="login">
+    <section>
       <div className="Back"> </div>
-      <div className="login-contents">
-        <div className="login-cont">
-          <div className="logo">
-            <a href="/">
-              <img src={logo} alt="Tracker Logo" />
-              <h1>Tracker</h1>
-            </a>
+      <div className="login">
+        <div className="login-contents">
+          <div className="login-cont">
+            <div className="logo">
+                <img src={logo} alt="Tracker Logo" />
+                <h1>Tracker</h1>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="content">
-        <div className="form-cont">
-          <div className="newusr">
-            <h1>
-              New user? <a href="/">signUp</a>
-            </h1>
-          </div>
-          <form action="" method="POST">
-            <input type="text" name="uname" placeholder="Username or Email" />
-            <br />
-            <input type="text" name="pass" placeholder="Password" />
-            <div className="btn_login">
-              <a href="/">Login</a>
-              <h4>forget Password?</h4>
+        <div className="content">
+          <div className="form-cont">
+            <div className="newusr">
+              <h1>
+                New user? <a href="/">signUp</a>
+              </h1>
             </div>
-          </form>
+            <form action="" method="POST">
+              <input type="text" name="uname" placeholder="Username or Email" />
+              <br />
+              <input type="text" name="pass" placeholder="Password" />
+              <div className="btn_login">
+                <a href="/">Login</a>
+              </div>
+              <h4 className="forget">Forget Password?</h4>
+            </form>
+          </div>
         </div>
       </div>
     </section>
