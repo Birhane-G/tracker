dff1ed781b8e19ff2cf723916f67551cce1ea490
Birhane <Birhanegebrial1@gmail.com>
2022-09-21 16:14:24 +0300

    Dashboard Sidebar

--- src/Dashboard/inc/Sidebar.js
+++ src/Dashboard/inc/Sidebar.js
@@ -5,7 +5,25 @@ function Sidebar() {
     <section>
       <div className="sidebar">
         <div className="side-top-logo-container"></div>
-        <div></div>
+        <div className="side-bar-content">
+          <div className="side-bar-center-content">
+            <div className="side-bar-top-content-nav">
+              <ul>
+              <a href="/"><li>Dashboard</li></a>
+              <a href="/"><li>Visitor</li></a>
+              <a href="/"><li>PageView</li></a>
+              <a href="/"><li>Device</li></a>
+              <a href="/"><li>Location</li></a>
+              </ul>
+            </div>
+            <div className="side-bar-center-content-nav">
+              <ul>
+              <a href="/"><li>Dashboard</li></a>
+              <a href="/"><li>Setting</li></a>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
