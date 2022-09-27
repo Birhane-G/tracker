0c34e000721fbf37b82b98c16c0cf23e6695f93f
Gemechisgk <Gemechis.gk@gmail.com>
2022-09-27 12:15:42 +0300

    device page fix

--- src/Dashboard/pages/Device.js
+++ src/Dashboard/pages/Device.js
@@ -1,39 +1,41 @@
 import React from "react";
 import { Sidebar } from "./Dashboard";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+
 
 import "../style/device.css";
-function Device() {
-  return (
-    <>
-      <div className="Dashboard-include-content">
-        <div className="dashboard-sidebar">
-          <Sidebar />
-        </div>
-        <div className="dashboard-content">
-          <AllPageview />
-        </div>
-      </div>
-    </>
-  );
-}
-export function AllPageview() {
-  return (
-    <div className="cards-contents">
-      <div className="device-cards">
-        <div className="device type">
-          <h1>Device type</h1>
-        </div>
-        <div className="device brand">
-          <h1>Device brand</h1>
-        </div>
-        <div className="device model">
-          <h1>Device model</h1>
-        </div>
-        <div className="device screenResolution">
-          <h1>Screen resolution</h1>
-        </div>
-      </div>
-    </div>
-  );
-}
-export default Device;
+ function Device() {
+   return (
+     <>
+       <div className="Dashboard-include-content">
+         <div className="dashboard-sidebar">
+           <Sidebar />
+         </div>
+         <div className="dashboard-content">
+           <AllPageview />
+         </div>
+       </div>
+     </>
+   );
+ }
+ export function AllPageview() {
+   return (
+     <div className="cards-contents">
+       <div className="device-cards">
+         <div className="device type">
+           <h1>Device type</h1>
+         </div>
+         <div className="device brand">
+           <h1>Device brand</h1>
+         </div>
+         <div className="device model">
+           <h1>Device model</h1>
+         </div>
+         <div className="device screenResolution">
+           <h1>Screen resolution</h1>         </div>
+       </div>
+     </div>
+   );
+ }
+
+ export default Device;
