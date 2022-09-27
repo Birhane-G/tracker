9c34457e22fe170f9e26a440e5101c1ac2c810fa
Birhane <Birhanegebrial1@gmail.com>
2022-09-27 22:33:51 +0300

    new changes in the dashboard sidebar

--- src/Dashboard/pages/Pageview.js
+++ src/Dashboard/pages/Pageview.js
@@ -92,14 +92,11 @@ const data = [
 ];
 function Pageview() {
   return (
-
-      <div className="Dashboard-include-content">
-        <div className="dashboard-content">
+      <div className="pageview-include-content">
           <PageViewPerformance />
           <div className="other-contents">
             <PageView />
             <Other />
-          </div>
         </div>
       </div>
   );
@@ -121,7 +118,7 @@ export function PageViewPerformance() {
                 <CartesianGrid strokeDasharray="7 1" />
                 <XAxis dataKey="date" />
                 <YAxis dataKey="time" />
-                <Tooltip style={{ x: 0, y: 0, width: 100, height: 100 }} />
+                <Tooltip style={{ x: 0, y: 0, width: 100, height: 50 }} />
                 <Legend />
                 <Line type="" dataKey="Avg page load Time" stroke="#8884d8" />
                 <Line type="" dataKey="Avg Network Time" stroke="#82ca9d" />
