9a9c96c393e6f98aca7bea6c55000270cec73539
Birhane <Birhanegebrial1@gmail.com>
2022-09-30 20:19:38 +0300

    Dashboard visitor over day added

---
+++ src/Dashboard/Components/Dashboard/VisirorsT.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {Line} from "react-chartjs-2"
+import Chart from "chart.js/auto";
+export const VisirorsT = () => {
+  return (
+    <div className="Visitor-over-time">
+      <div>Visirors over Day</div>
+      <div className="datas-container">
+        <VisirorsODD />
+      </div>
+    </div>
+  );
+};
+
+const VisirorsODD = () => {
+    const labels = [
+      "Mon",
+      "tus",
+      "wed",
+      "tur",
+      "fri",
+      "sat",
+      "sun",
+      
+    ];
+  
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "visits",
+          backgroundColor: "#dc105a",
+          borderColor: "#dc105a",
+          data: [0.15, 0.2, 0.7, 0.4, 1, 0.77, 0.57],
+        },
+      ],
+  };
+  return (
+    <div className="data">
+      <Line width={"10%"} height={2} data={data}/>
+    </div>
+  )
+}
+
