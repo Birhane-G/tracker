ccf0f09208ffabff422ed6c1df37ad1855972810
Birhane <Birhanegebrial1@gmail.com>
2022-09-30 09:55:44 +0300

    Channel Type

--- src/Dashboard/Charts/Visitor/Piechart.js
+++ src/Dashboard/Charts/Visitor/Piechart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import "../../style/chart.css"
 export const Piechart = () => {
@@ -22,9 +22,9 @@ export const Piechart = () => {
   return (
     <div className='real-time-visitor-pie-chart'>
           <div className='real-time-text'>
-            <h3>Last 24 hours</h3>
+            <h3>Last 30m</h3>
           </div>
-        <Pie data={data} />
+        <Doughnut data={data} />
     </div>
   );
 };
--- src/Dashboard/pages/Visitor.js
+++ src/Dashboard/pages/Visitor.js
@@ -5,15 +5,66 @@ import { Piechart } from "../Charts/Visitor/Piechart";
 function Visitor() {
   return (
     <div className="Visitor-content">
-      <h1>Visitor page</h1>
       <div className="Visitor-contaner">
-        <div className="Visitor-Real-time"></div>
+        <div className="Visitor-log">
+          <Visitorlog />
+        </div>
         <div className="Real-Time">
           <Piechart />
+          <ChanalType />
         </div>
       </div>
     </div>
   );
 }
+
+export const ChanalType = () => {
+  return (
+    <div className="Visitor-Channel-type">
+      <div>
+        <h3>Channel Type</h3>
+      </div>
+      <div>
+        <table className="channel-type-table">
+          <thead>
+            <tr>
+              <th>Channel</th>
+              <th>Visits</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Google</td>
+              <td>200</td>
+            </tr>
+            <tr>
+              <td>linkedin</td>
+              <td>300</td>
+            </tr>
+            <tr>
+              <td>facebook</td>
+              <td>200</td>
+            </tr>
+            <tr>
+              <td>instagram</td>
+              <td>200</td>
+            </tr>
+            <tr>
+              <td>reddit</td>
+              <td>200</td>
+            </tr>
+            <tr>
+              <td>twitter</td>
+              <td>200</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export const Visitorlog = () => {
+  return <div>Visitor log</div>;
+};
 
 export default Visitor;
