2d17f80b5255b8e8e43ac19a90879ac200b96335
Birhane <Birhanegebrial1@gmail.com>
2022-10-01 20:06:11 +0300

    Browser

--- src/Dashboard/Software/Software.js
+++ src/Dashboard/Software/Software.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./style/software.css"
-
+import "./style/software.css";
+import Browser from "./components/Browser";
 export const Software = () => {
   return (
     <div className="software-container">
-      <div className="one"></div>
+      <div className="one">
+        <Browser />
+      </div>
       <div className="one"></div>
       <div className="one"></div>
       <div className="one"></div>
--- src/Dashboard/Software/components/Browser.js
+++ src/Dashboard/Software/components/Browser.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Firefox from "../icons/browsers/FF.png";
+import chrome from "../icons/browsers/CF.png";
+import opera from "../icons/browsers/OH.png";
+import IE from "../icons/browsers/IE.png";
 
 const Browser = () => {
   return (
@@ -7,21 +11,40 @@ const Browser = () => {
         <h3>Browser</h3>
       </div>
       <div className="Browser-data">
-        <table cellSpacing={0} cellPadding={0} >
+        <table cellSpacing={0} cellPadding={0} className="Browser-data-table">
           <thead>
             <tr>
               <th>Browser</th>
               <th>Visitors</th>
             </tr>
           </thead>
-          <tbod>
+          <tbody>
             <tr>
-              <td></td>
-              <td></td>
+              <td>
+                <img src={Firefox} className="icons"/> Firefox
+              </td>
+              <td>1200</td>
             </tr>
-          </tbod>
+            <tr>
+              <td>
+                <img src={chrome} className="icons"/> chrome
+              </td>
+              <td>1200</td>
+            </tr>
+            <tr>
+              <td>
+                <img src={opera} className="icons"/> opera
+              </td>
+              <td>1200</td>
+            </tr>
+            <tr>
+              <td>
+                <img src={IE} className="icons"/> Internet Explore
+              </td>
+              <td>1200</td>
+            </tr>
+          </tbody>
         </table>
-
       </div>
     </div>
   );
