b5c58a9260de6b59f236681343decd3615bdde6f
Birhane <Birhanegebrial1@gmail.com>
2022-10-01 21:25:39 +0300

    Os component added

--- src/Dashboard/Software/Software.js
+++ src/Dashboard/Software/Software.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./style/software.css";
 import Browser from "./components/Browser";
+import {OperatingS} from "./components/OperatingS";
+
+
 export const Software = () => {
   return (
     <div className="software-container">
       <div className="one">
         <Browser />
       </div>
-      <div className="one"></div>
+      <div className="one">
+        <OperatingS />
+      </div>
       <div className="one"></div>
       <div className="one"></div>
     </div>
