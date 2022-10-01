4107b17104ff72f141ef3a4033e3846485d0c10b
Birhane <Birhanegebrial1@gmail.com>
2022-10-01 19:28:08 +0300

    change folder stracture and add some icons and change software page

---
+++ src/Dashboard/Software/Software.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style/software.css"
+
+export const Software = () => {
+  return (
+    <div className="software-container">
+      <div className="one"></div>
+      <div className="one"></div>
+      <div className="one"></div>
+      <div className="one"></div>
+    </div>
+  );
+};
