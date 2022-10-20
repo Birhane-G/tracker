5864e591850fa5e30af2d2a913a5c3a69baec2e9
Birhane <Birhanegebrial1@gmail.com>
2022-10-20 17:47:34 +0300

    Visitor Log Data Request to the server

--- src/Dashboard/Visitor/components/Visitorlog.js
+++ src/Dashboard/Visitor/components/Visitorlog.js
@@ -1,31 +1,66 @@
-import React from "react";
-import Et from "../icons/flags/et.png";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import Et from "";
+
 export const Visitorlog = () => {
+  const [Visitors, setVisitors] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get("http://192.168.1.11:8000/api/VisitorLog").then((result) => {
+      if (result.data.status === 200) {
+        setLoading(false);
+        setVisitors(result.data.value);
+      }
+    });
+  }, []);
+  var Datas = "";
+  if (loading) {
+    Datas = (
+          <h3>LOADING....</h3> 
+    );
+  } else {
+    Datas = Visitors.map((item) => {
+      return (
+        <div className="visitor-log-Content">
+        <div className="Left" >
+          <div className="Country-Name">
+            <img src={require(`../../${item.countryFlag}`)} alt=""/>
+            <h4>{item.location}</h4>
+          </div>
+          <div className="Channel-Type">
+            <h4>{item.referrerTypeName}</h4>
+          </div>
+          <div className="IP-Address">
+            <h4>{item.visitIp}</h4>
+          </div>
+          <div className="Date">
+            <h4>{item.serverDatePrettyFirstAction}</h4>
+          </div>
+          <div className="Time">
+            <h4>{item.visitLocalTime}</h4>
+          </div>
+        </div>
+        <div className="center">
+          <div className="Software">
+          <img src={require(`../../${item.browserIcon}`)} alt=""/><br/>
+          <img src={require(`../../${item.deviceTypeIcon}`)} alt=""/><br/>
+          <img src={require(`../../${item.operatingSystemIcon}`)} alt=""/><br/>
+          {/* <img src={require(`../../${item.visitorTypeIcon}`)} alt=""/> */}
+          </div>
+        </div>
+        <div className="right">
+          <div className="Software">
+          </div>
+        </div>
+        </div>
+      );
+    })
+  }
   return (
     <div className="table-title">
       <h3>Visitor log</h3>
-      <div className="visitor-log-Content">
-        <div className="Left">
-          <div className="Country-Name">
-            <img src={Et} alt="" />
-            <h4>Ethiopia</h4>
-          </div>
-          <div className="Channel-Type">
-            <h4>Direct Entry</h4>
-          </div>
-          <div className="IP-Address">
-            <h4>Ip: 127.0.0.0</h4>
-          </div>
-          <div className="Date">
-            <h4>wednesday, october 19,2022</h4>
-          </div>
-          <div className="Time">
-            <h4>18.11.10</h4>
-          </div>
-        </div>
-        <div className="Right">
-
-        </div>
+      <div>
+        {Datas}
       </div>
     </div>
   );
