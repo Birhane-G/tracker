f39d3d39507687cb570abca301eab56d59edaea9
birhane <birhanegebrial1@gmail.com>
2022-11-15 14:49:18 -0500

    Birhane

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,16 @@
 import logo from "../images/Trackerlogo.png";
 function Login() {
   return (
-    <section>
-      <div className="Back"> </div>
+    <section className="try">
+      <div className="Back-login"> </div>
       <div className="login">
         <div className="login-contents">
           <div className="login-cont">
             <div className="logo">
-              <img src={logo} alt="Tracker Logo" />
-              <h1>Tracker</h1>
+              <a href="/">
+                <img src={logo} alt="Tracker Logo" />
+                <h1>Tracker</h1>
+              </a>
             </div>
           </div>
         </div>
@@ -16,18 +18,16 @@ function Login() {
           <div className="form-cont">
             <div className="newusr">
               <h1>
-                New user? <a href="/registration">signUp</a>
+                New user? <a href="/registration">Signup</a>
               </h1>
             </div>
             <input type="text" name="uname" placeholder="Email" />
             <br />
             <input type="password" name="pass" placeholder="Password" />
-            <div className="btn_login">
-              <a href="/dashboard">
-                <button className="btn-login">Login</button>
-              </a>
-            </div>
-            <h4 className="forget">Forget Password?</h4>
+              <div className="btn-login">
+                <a href="/">Login</a>
+              </div>
+            <a href="/"><h4 className="forget">Forget Password?</h4></a>
           </div>
         </div>
       </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import "../Styles/register_style.css";
+import React from "react";
 import logo from "../images/Trackerlogo.png";
 
 function Register() {
   return (
     <section>
-        <div className="Back"> </div>
-       <div className='register'>
-       <div className="register-contents">
+      <div className="Back"> </div>
+      <div className="register">
+        <div className="register-content">
           <div className="register-cont">
             <div className="logo">
+              <a href="/">
                 <img src={logo} alt="Tracker Logo" />
                 <h1>Tracker</h1>
+              </a>
             </div>
           </div>
         </div>
         <div className="content">
-          <div className="form-cont">
+          <div className="form-reg-cont">
             <form action="" method="POST">
               <input type="text" name="fname" placeholder="First Name" />
               <input type="text" name="lname" placeholder="Last Name" />
@@ -25,12 +26,19 @@ function Register() {
               <br />
               <input type="password" name="pass" placeholder="Password" />
               <br />
-              <input type="password" name="cpass" placeholder="Confirm Password" />
-              <input type="text" name="weburl" placeholder="Input Website URL" />
+              <input
+                type="password"
+                name="cpass"
+                placeholder="Confirm Password"
+              /><br />
+              <input
+                type="text"
+                name="weburl"
+                placeholder="Input Website URL"
+              />
               <div className="btn_register">
                 <a href="/">Register</a>
               </div>
-              
             </form>
             <div className="usr">
               <h1>
@@ -38,10 +46,9 @@ function Register() {
               </h1>
             </div>
           </div>
-       </div>
-       </div>
+        </div>
+      </div>
     </section>
-    
-  )
+  );
 }
 export default Register;
