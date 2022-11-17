539caacb79bf40e613ca79161d8d1b5ddb44da85
Gemechisgk <gemechis.gk@gmail.com>
2022-11-17 06:12:16 +0300

    profile picture

--- src/Dashboard/inc/Sidebar.js
+++ src/Dashboard/inc/Sidebar.js
@@ -51,8 +51,8 @@ const Sidebar = ({ children }) => {
     <div className="sidebar">
       <motion.div
         animate={{ width: Press ? "15%" : "50px" }}
-        className="side-bar-content"
-      >
+        className="side-bar-content">
+      
         <div className="side-bar-content">
           <div className="menu-con">
             <div className="menu-back"></div>
@@ -62,40 +62,16 @@ const Sidebar = ({ children }) => {
               className="menu"
             />
           </div>
-          <div className="side-top-logo-container">
-            {" "}
-            <nav>
-              <ul>
-                <li class="sub-menu-parent" tab-index="0">
-                  <a href="#" class="sub-menu-title">
-                    Websites
-                  </a>
-                  <ul class="sub-menu">
-                    <li>
-                      <a href="#" class="Drop1">
-                        #1 TestSite
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="Drop2">
-                        #2 TestSite
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="Drop3">
-                        #3 TestSite
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="Drop4">
-                        #4 TestSite
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <div className="side-top-logo-container">  <nav>
+      <div class="profile-block"> 
+       <div class="profile-picture big-profile-picture clear">
+          <img width="90px" height="80" alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Abebe_Bikila%2C_1972_card.jpg/461px-Abebe_Bikila%2C_1972_card.jpg?20160711053925" ></img>
+        </div>
+            <h1 class="user-name">Abebe Bikila</h1>
+              <p class="site-title">testwebsite.com</p>                                    
+                </div>
+ </nav>
+ </div>
           <div className="side-bar-center-content">
             <div className="side-bar-top-content-nav">
               <ul>
@@ -113,6 +89,7 @@ const Sidebar = ({ children }) => {
         </div>
       </motion.div>
       <main>{children}</main>
+     
     </div>
   );
 };
